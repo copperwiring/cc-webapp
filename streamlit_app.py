1491d4216cfc6c3547323e5b4e5adf70e4d636fc
copperwiring <srishtiyadav0807@gmail.com>
2024-09-14 20:03:06 +0200

    move page config

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ from openai import OpenAI
 from PIL import Image
 import streamlit as st
 
+st.set_page_config(page_title="DALL.E 3 Image Generation")
+
 st.title("🎈 My new app here")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
@@ -23,7 +25,6 @@ def generate_image(image_description):
   img_url = response.data[0].url
   return img_url
 
-st.set_page_config(page_title="DALL.E 3 Image Generation")
 
 
 st.title('DALL.E 3 Image Generation')
